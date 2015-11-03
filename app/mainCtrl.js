b13b6579d682bcaaac405ac57bf1e71a19219c30
Kyle <kyle.lauritzen@gmail.com>
2015-11-03 16:13:52 -0700

    on step 4

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -8,13 +8,38 @@ app.controller('mainCtrl', function($scope){
     
     $scope.canShow = true;
     
-    $scope.user = {
-        
-        name: "Geoff McMammy",
-        age: 43,
-        email: "geoffdude@gmail.com"
-        
-    }
+    $scope.users = [
+ {
+   name: "Geoff McMammy",
+   age: 43,
+   email: "geofdude@gmail.com",
+   city: "Provo"
+ },
+ {
+   name: "Frederick Deeder",
+   age: 26,
+   email: "fredeed@gmail.com",
+   city: "Austin"
+ },
+ {
+   name: "Spencer Rentz",
+   age: 35,
+   email: "spencerrentz@gmail.com",
+   city: "Sacramento"
+ },
+ {
+   name: "Geddup Ngo",
+   age: 43,
+   email: "geddupngo@gmail.com",
+   city: "Orlando"
+ },
+ {
+   name: "Donst Opbie Leevin",
+   age: 67,
+   email: "gernee@gmail.com",
+   city: "Phoenix"
+ }
+];
     
     
 })
